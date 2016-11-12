ca00b9bb738f6e95fff1fedb21f1da4f25a53b29
Ben Cawkwell <bencawkwell@gmail.com>
2016-11-12 20:51:08 +0100

    First iteration of the client side application. Currently it just renders a board with a static game state. I chose to use a table, with the idea that navigating a table with screen readers is probably more intuitive than a list. I settled on using buttons as opposed to radio inputs, since visually it makes more sense, and I thought changing the look of radio inputs too drastically was actually worse for usability and accessibility.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+/**
+ * React component for each square on the tic-tac-toe board
+ */
+function Square(props) {
+  let cellContent;
+  if (props.data.value) {
+    cellContent = <div className="occupied">{props.data.value}</div>;
+  } else {
+    cellContent = (
+      <input className="available"
+        type="button"
+        name="move"
+        value={props.data.id} />
+    );
+  }
+  return (
+    <td className="square">
+      {cellContent}
+    </td>
+  );
+}
+
+/**
+ * React component for the entire tic-tac-toe board
+ */
+function Board(props) {
+  return (
+    <table className="board">
+      <tbody>
+        <tr>
+          <Square data={props.gameState[0]} />
+          <Square data={props.gameState[1]} />
+          <Square data={props.gameState[2]} />
+        </tr>
+        <tr>
+          <Square data={props.gameState[3]} />
+          <Square data={props.gameState[4]} />
+          <Square data={props.gameState[5]} />
+        </tr>
+        <tr>
+          <Square data={props.gameState[6]} />
+          <Square data={props.gameState[7]} />
+          <Square data={props.gameState[8]} />
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+/**
+ * React component to start/reset the game
+ */
+function Control(props) {
+  return (
+    <input type="button" value="New Game" />
+  );
+}
+
+/**
+ * Main Application
+ */
+class App extends React.Component {
   render() {
+    const gameState = [
+      {id: 1},
+      {id: 2},
+      {id: 3, value: 'X'},
+      {id: 4},
+      {id: 5, value: 'O'},
+      {id: 6, value: 'X'},
+      {id: 7},
+      {id: 8},
+      {id: 9, value: 'O'}
+    ];
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form className="App">
+        <Board gameState={gameState} />
+        <Control />
+      </form>
     );
   }
 }
