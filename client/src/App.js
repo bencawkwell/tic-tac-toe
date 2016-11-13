b971c369a92840d1618d9ceaf2580f119a6be926
Ben Cawkwell <bencawkwell@gmail.com>
2016-11-13 20:58:15 +0100

    Settled on using the term "tile" for each of the nine squares in the frontend application, and made the list of those tiles a property under a single game object in state. This is mostly to consolidate the entire game into one object to make changes to it easier.

--- client/src/App.js
+++ client/src/App.js
@@ -12,23 +12,23 @@ class Square extends React.Component {
 
   handleClick(e) {
     e.preventDefault();
-    this.props.handleMove(e.target.value);
+    this.props.handleMove(parseInt(e.target.value));
   }
 
   render() {
     let cellContent;
-    if (this.props.data.value) {
-      cellContent = <div className="occupied">{this.props.data.value}</div>;
+    if (this.props.tile.value) {
+      cellContent = <div className="occupied">{this.props.tile.value}</div>;
     } else {
       cellContent = (
         <button className="available"
           name="move"
           onClick={this.handleClick}
-          value={this.props.data.id} />
+          value={this.props.tile.id} />
       );
     }
     return (
-      <td className="square">
+      <td className="tile">
         {cellContent}
       </td>
     );
@@ -43,19 +43,19 @@ function Board(props) {
     <table className="board">
       <tbody>
         <tr>
-          <Square data={props.gameState[0]} handleMove={props.handleMove} />
-          <Square data={props.gameState[1]} handleMove={props.handleMove} />
-          <Square data={props.gameState[2]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[0]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[1]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[2]} handleMove={props.handleMove} />
         </tr>
         <tr>
-          <Square data={props.gameState[3]} handleMove={props.handleMove} />
-          <Square data={props.gameState[4]} handleMove={props.handleMove} />
-          <Square data={props.gameState[5]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[3]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[4]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[5]} handleMove={props.handleMove} />
         </tr>
         <tr>
-          <Square data={props.gameState[6]} handleMove={props.handleMove} />
-          <Square data={props.gameState[7]} handleMove={props.handleMove} />
-          <Square data={props.gameState[8]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[6]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[7]} handleMove={props.handleMove} />
+          <Square tile={props.tiles[8]} handleMove={props.handleMove} />
         </tr>
       </tbody>
     </table>
@@ -81,31 +81,31 @@ class App extends React.Component {
     this.handleMove = this.handleMove.bind(this);
 
     this.state = {
-      game: [
-        {id: 1},
-        {id: 2},
-        {id: 3},
-        {id: 4},
-        {id: 5},
-        {id: 6},
-        {id: 7},
-        {id: 8},
-        {id: 9}
-      ]
-    }
+      game: {
+        id: 'some-uuid-here',
+        tiles: [
+          {id: 1},
+          {id: 2},
+          {id: 3},
+          {id: 4},
+          {id: 5},
+          {id: 6},
+          {id: 7},
+          {id: 8},
+          {id: 9}
+        ]
+      }
+    };
   }
 
-  handleMove(position) {
-    const gameState = this.state.game.slice();
 
-    gameState[position-1].value = 'X';
-    this.setState({game: gameState});
+  handleMove(tileId) {
   }
 
   render() {
     return (
       <form className="App">
-        <Board gameState={this.state.game} handleMove={this.handleMove} />
+        <Board tiles={this.state.game.tiles} handleMove={this.handleMove} />
         <Control />
       </form>
     );
